6b03dceee6164e5d1cfbfd142496c714c5ca8769
allenzeng123 <zlk8550081@163.com>
2016-06-28 01:20:01 +0800

    from6 commit6

--- src/main/java/laobi/git/springTransaction/entity/Persion.java
+++ src/main/java/laobi/git/springTransaction/entity/Persion.java
@@ -16,8 +16,8 @@ public class Persion {//60001---60003--604
 	 */
 	private static final long serialVersionUID = 5127853981202454040L;//605
 	private int id;
-	private String name;
-	private Integer age;
+	private String name;//from6 commit6
+	private Integer age;//from6 commit7
 	private String telephone;
 
 	public Persion() {
